685025e10f53f72102c4dcdf5b4937c4dac27491
Ayder90 <konohamaru2001@mail.ru>
2023-08-08 11:28:22 +0300

    Prog
    Prog

--- 11111.js
+++
@@ -1,6 +0,0 @@
-let first_text = document.querySelector('.first_text')
-first_text.innerHTML = first_text.textContent.replace(/\S/g, '<span>$&</span>')
-first_text.addEventListener('mouseover', function(event){
-    if (event.target.tagName != 'SPAN') return
-    event.target.classList.add('active')
-})
